0c77f3646056d84cbe0c342a3429f6dbd7490f3d
Музыка Владислав <ru9art@gmail.com>
2024-05-21 16:50:31 +0300

    feat: Add app version in swagger

--- apps/gateway/src/core/config/swagger/swaggerSetup.ts
+++ apps/gateway/src/core/config/swagger/swaggerSetup.ts
@@ -1,15 +1,41 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { APP_GLOBAL_PREFIX } from '../config.constants';
+import { ConfigService } from '@nestjs/config';
+
+const generateSwaggerDescription = () => `The intagram API description 
+<p>
+Download OpenAPI specification: <a href="https://funny-inctagram.site/api/v1/doc-json">swagger.json</a>
+</p>`;
 
 export function swaggerSetup(app: INestApplication) {
+  const configService = app.get<ConfigService>(ConfigService);
+
   const config = new DocumentBuilder()
     .addCookieAuth('refreshToken')
     .setTitle('Intagram backend')
-    .setDescription('The intagram API description')
-    .setVersion('1.0')
-    .addTag('Intagram')
-    .addBearerAuth()
+    .setDescription(generateSwaggerDescription())
+    .setVersion(configService.get('npm_package_version') ?? '1.0')
+    .addCookieAuth(
+      'refreshToken',
+      {
+        type: 'apiKey',
+        in: 'cookie',
+        name: 'refreshToken',
+        description: 'Refresh token for user',
+      },
+      'refreshToken',
+    )
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        description: 'JWT Authorization header using the Bearer scheme.',
+        in: 'header',
+      },
+      'accessToken',
+    )
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
